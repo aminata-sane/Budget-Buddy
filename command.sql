f1c81f29f548bb9575bf4bdd5dd6576753776e7b
aminata-sane <aminata-constance.sane@laplateforme.io>
2025-03-23 20:03:16 +0100

    update files
    git rebase --continue

---
+++ command.sql
@@ -0,0 +1,4 @@
+INSERT INTO banker_clients (ID_banker, id_client) VALUES (1, 1);
+INSERT INTO banker_clients (ID_banker, id_client) VALUES (1, 2);
+
+SELECT * FROM banker_clients;
--- menu.py
+++ menu.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, ImageDraw
 import os
 import hashlib
 from database import verify_client, add_banker, authenticate_banker, get_clients_of_banker, add_client  # Importer les fonctions nécessaires
@@ -18,32 +18,36 @@ class BankingApp:
         # Set background color
         self.root.configure(bg="white")
         
-        # Add logo and slogan
-        logo_label = tk.Label(self.root, text="Bank Logo", font=("Arial", 24), bg="white", fg="black")
-        logo_label.pack(pady=10)
-        
-        slogan_label = tk.Label(self.root, text="Your Friendly Bank !", font=("Arial", 16), bg="white", fg="black")
-        slogan_label.pack(pady=5)
-        
         self.frame = tk.Frame(self.root, bg="white")
         self.frame.pack(fill=tk.BOTH, expand=True)
         
         self.load_images()
         self.show_home()
     
-    def load_images(self):
-        self.client_photo = self.load_image("images/client.png", (100, 150))  # Adjust image size
-        self.banker_photo = self.load_image("images/bank.png", (100, 150))  # Adjust image size
-        self.inscription_photo = self.load_image("images/registration.png", (75, 75))  # Adjust image size
-        self.connexion_photo = self.load_image("images/login.png", (75, 75))  # Adjust image size
+    def round_corners(self, image, radius):
+        # Create a mask with rounded corners
+        mask = Image.new("L", image.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.rounded_rectangle((0, 0) + image.size, radius, fill=255)
+        image.putalpha(mask)
+        return image
     
-    def load_image(self, image_path, size):
+    def load_image(self, image_path, size, radius=20):
         if os.path.exists(image_path):
             img = Image.open(image_path).resize(size)
+            img = self.round_corners(img, radius)
+            print(f"Image loaded: {image_path}")  # Message de débogage
             return ImageTk.PhotoImage(img)
         else:
             print(f"Image not found: {image_path}")
             return None
+    
+    def load_images(self):
+        self.logo_photo = self.load_image("images/logo.png", (100, 100), radius=20)  # Adjust image size and radius
+        self.client_photo = self.load_image("images/client.png", (350, 450), radius=20)  # Adjust image size and radius
+        self.banker_photo = self.load_image("images/bank.png", (350, 450), radius=20)  # Adjust image size and radius
+        self.inscription_photo = self.load_image("images/registration.png", (75, 75), radius=20)  # Adjust image size and radius
+        self.connexion_photo = self.load_image("images/login.png", (75, 75), radius=20)  # Adjust image size and radius
     
     def clear_frame(self):
         for widget in self.frame.winfo_children():
@@ -51,18 +55,26 @@ class BankingApp:
     
     def show_home(self):
         self.clear_frame()
-        tk.Label(self.frame, text="Make your choice", font=("Arial", 14), bg="white", fg="black").pack(pady=20)
+        
+        logo_label = tk.Label(self.frame, image=self.logo_photo, bg="white")
+        logo_label.grid(row=0, column=0, padx=10, pady=10, sticky="nw")
+        
+        tk.Label(self.frame, text="Welcome to Your Friendly Bank!", font=("Arial", 20, "bold"), bg="white", fg="black").grid(row=0, column=1, columnspan=2, pady=20)
+        
+        self.frame.columnconfigure(0, weight=1, minsize=100)
+        self.frame.columnconfigure(1, weight=1, minsize=100)
+        self.frame.rowconfigure(1, weight=1, minsize=150)
         
         client_button = tk.Button(self.frame, image=self.client_photo, command=self.show_client_login, borderwidth=0, bg="white")
-        client_button.pack(side=tk.LEFT, padx=20, pady=10)
-        client_label = tk.Label(self.frame, text="Customer", font=("Arial", 12), cursor="hand2", bg="white", fg="black")
-        client_label.pack(side=tk.LEFT, padx=20)
+        client_button.grid(row=1, column=0, padx=20, pady=10, sticky="nsew")
+        client_label = tk.Label(self.frame, text="✨Customer✨", font=("Arial", 25), cursor="hand2", bg="white", fg="black")
+        client_label.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
         client_label.bind("<Button-1>", lambda event: self.show_client_login())
 
         banker_button = tk.Button(self.frame, image=self.banker_photo, command=self.show_banker_page, borderwidth=0, bg="white")
-        banker_button.pack(side=tk.RIGHT, padx=20, pady=10)
-        banker_label = tk.Label(self.frame, text="Banker", font=("Arial", 12), cursor="hand2", bg="white", fg="black")
-        banker_label.pack(side=tk.RIGHT, padx=20)
+        banker_button.grid(row=1, column=1, padx=20, pady=10, sticky="nsew")
+        banker_label = tk.Label(self.frame, text="✨Banker✨", font=("Arial", 25), cursor="hand2", bg="white", fg="black")
+        banker_label.grid(row=2, column=1, padx=20, pady=10, sticky="nsew")
         banker_label.bind("<Button-1>", lambda event: self.show_banker_page())
     
     def show_client_login(self):
@@ -70,21 +82,21 @@ class BankingApp:
         tk.Label(self.frame, text="Client Login Page", font=("Arial", 14), bg="white", fg="black").pack(pady=20)
         
         inscription_button = tk.Button(self.frame, image=self.inscription_photo, command=self.show_inscription_client, bg="white", bd=0)
-        inscription_button.pack(side=tk.LEFT, padx=20, pady=10)
-        tk.Label(self.frame, text="Registration", font=("Arial", 12), bg="white", fg="black").pack(side=tk.LEFT, padx=20)
+        inscription_button.pack(side=tk.LEFT, padx=20, pady=10, expand=True, fill=tk.BOTH)
+        tk.Label(self.frame, text="Registration", font=("Arial", 12), bg="white", fg="black").pack(side=tk.LEFT, padx=20, expand=True, fill=tk.BOTH)
 
         connexion_button = tk.Button(self.frame, image=self.connexion_photo, command=self.show_connexion_client, bg="white", bd=0)
-        connexion_button.pack(side=tk.RIGHT, padx=20, pady=10)
-        tk.Label(self.frame, text="Login", font=("Arial", 12), bg="white", fg="black").pack(side=tk.RIGHT, padx=20)
+        connexion_button.pack(side=tk.RIGHT, padx=20, pady=10, expand=True, fill=tk.BOTH)
+        tk.Label(self.frame, text="Login", font=("Arial", 12), bg="white", fg="black").pack(side=tk.RIGHT, padx=20, expand=True, fill=tk.BOTH)
 
-        tk.Button(self.frame, text="Back to Home", command=self.show_home, bg="white", fg="black", bd=0).pack(pady=10)
+        tk.Button(self.frame, text="Back to Home", command=self.show_home, bg="white", fg="black", bd=0).pack(pady=10, expand=True, fill=tk.BOTH)
     
     def show_banker_page(self):
         self.clear_frame()
         tk.Label(self.frame, text="Choose your action", font=("Arial", 14), bg="white", fg="black").pack(pady=20)
-        tk.Button(self.frame, text="Registration", command=self.show_banker_registration, bg="white", fg="black").pack(pady=10)
-        tk.Button(self.frame, text="Login", command=self.show_banker_login, bg="white", fg="black").pack(pady=10)
-        tk.Button(self.frame, text="Back", command=self.show_home, bg="white", fg="black").pack(pady=10)
+        tk.Button(self.frame, text="Registration", command=self.show_banker_registration, bg="white", fg="black").pack(pady=10, expand=True, fill=tk.BOTH)
+        tk.Button(self.frame, text="Login", command=self.show_banker_login, bg="white", fg="black").pack(pady=10, expand=True, fill=tk.BOTH)
+        tk.Button(self.frame, text="Back", command=self.show_home, bg="white", fg="black").pack(pady=10, expand=True, fill=tk.BOTH)
     
     def show_banker_registration(self):
         self.clear_frame()
@@ -93,22 +105,22 @@ class BankingApp:
         
         tk.Label(self.frame, text="Name:", bg="white", fg="black").pack()
         self.name_entry = tk.Entry(self.frame)
-        self.name_entry.pack()
+        self.name_entry.pack(expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Surname:", bg="white", fg="black").pack()
         self.surname_entry = tk.Entry(self.frame)
-        self.surname_entry.pack()
+        self.surname_entry.pack(expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Email:", bg="white", fg="black").pack()
         self.email_entry = tk.Entry(self.frame)
-        self.email_entry.pack()
+        self.email_entry.pack(expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Password:", bg="white", fg="black").pack()
         self.password_entry = tk.Entry(self.frame, show="*")
-        self.password_entry.pack()
+        self.password_entry.pack(expand=True, fill=tk.BOTH)
         
-        tk.Button(self.frame, text="Register", command=self.register_banker, bg="white", fg="black").pack(pady=10)
-        tk.Button(self.frame, text="Back", command=self.show_banker_page, bg="white", fg="black").pack(pady=10)
+        tk.Button(self.frame, text="Register", command=self.register_banker, bg="white", fg="black").pack(pady=10, expand=True, fill=tk.BOTH)
+        tk.Button(self.frame, text="Back", command=self.show_banker_page, bg="white", fg="black").pack(pady=10, expand=True, fill=tk.BOTH)
     
     def register_banker(self):
         name = self.name_entry.get()
@@ -132,14 +144,14 @@ class BankingApp:
         
         tk.Label(self.frame, text="Email:", bg="white", fg="black").pack()
         self.login_email_entry = tk.Entry(self.frame)
-        self.login_email_entry.pack()
+        self.login_email_entry.pack(expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Password:", bg="white", fg="black").pack()
         self.login_password_entry = tk.Entry(self.frame, show="*")
-        self.login_password_entry.pack()
+        self.login_password_entry.pack(expand=True, fill=tk.BOTH)
         
-        tk.Button(self.frame, text="Login", command=self.login_banker, bg="white", fg="black").pack(pady=10)
-        tk.Button(self.frame, text="Back", command=self.show_banker_page, bg="white", fg="black").pack(pady=10)
+        tk.Button(self.frame, text="Login", command=self.login_banker, bg="white", fg="black").pack(pady=10, expand=True, fill=tk.BOTH)
+        tk.Button(self.frame, text="Back", command=self.show_banker_page, bg="white", fg="black").pack(pady=10, expand=True, fill=tk.BOTH)
     
     def login_banker(self):
         email = self.login_email_entry.get()
@@ -167,22 +179,22 @@ class BankingApp:
         
         tk.Label(self.frame, text="Name:", font=("Arial", 12), bg="white", fg="black").pack(pady=5)
         self.nom_entry = tk.Entry(self.frame)
-        self.nom_entry.pack(pady=5)
+        self.nom_entry.pack(pady=5, expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Surname:", font=("Arial", 12), bg="white", fg="black").pack(pady=5)
         self.prenom_entry = tk.Entry(self.frame)
-        self.prenom_entry.pack(pady=5)
+        self.prenom_entry.pack(pady=5, expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Email:", font=("Arial", 12), bg="white", fg="black").pack(pady=5)
         self.email_entry = tk.Entry(self.frame)
-        self.email_entry.pack(pady=5)
+        self.email_entry.pack(pady=5, expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Password:", font=("Arial", 12), bg="white", fg="black").pack(pady=5)
         self.password_entry = tk.Entry(self.frame, show="*")
-        self.password_entry.pack(pady=5)
+        self.password_entry.pack(pady=5, expand=True, fill=tk.BOTH)
         
-        tk.Button(self.frame, text="Register", command=self.register_client, bg="white", fg="black", bd=0).pack(pady=10)
-        tk.Button(self.frame, text="Back", command=self.show_client_login, bg="white", fg="black", bd=0).pack(pady=10)
+        tk.Button(self.frame, text="Register", command=self.register_client, bg="white", fg="black", bd=0).pack(pady=10, expand=True, fill=tk.BOTH)
+        tk.Button(self.frame, text="Back", command=self.show_client_login, bg="white", fg="black", bd=0).pack(pady=10, expand=True, fill=tk.BOTH)
     
     def register_client(self):
         name = self.nom_entry.get()
@@ -205,14 +217,14 @@ class BankingApp:
         
         tk.Label(self.frame, text="Email:", font=("Arial", 12), bg="white", fg="black").pack(pady=5)
         self.email_entry = tk.Entry(self.frame)
-        self.email_entry.pack(pady=5)
+        self.email_entry.pack(pady=5, expand=True, fill=tk.BOTH)
         
         tk.Label(self.frame, text="Password:", font=("Arial", 12), bg="white", fg="black").pack(pady=5)
         self.password_entry = tk.Entry(self.frame, show="*")
-        self.password_entry.pack(pady=5)
+        self.password_entry.pack(pady=5, expand=True, fill=tk.BOTH)
         
-        tk.Button(self.frame, text="Login", command=self.login_client, bg="white", fg="black", bd=0).pack(pady=10)
-        tk.Button(self.frame, text="Back", command=self.show_client_login, bg="white", fg="black", bd=0).pack(pady=10)
+        tk.Button(self.frame, text="Login", command=self.login_client, bg="white", fg="black", bd=0).pack(pady=10, expand=True, fill=tk.BOTH)
+        tk.Button(self.frame, text="Back", command=self.show_client_login, bg="white", fg="black", bd=0).pack(pady=10, expand=True, fill=tk.BOTH)
     
     def login_client(self):
         email = self.email_entry.get()
